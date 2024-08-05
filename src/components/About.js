8cfc84c00bc2a2041a487b8970c986ebb64bab5c
Gurupatel007 <98084018+Gurupatel007@users.noreply.github.com>
2024-08-05 21:21:40 +0530

    update the about section

--- src/components/About.js
+++ src/components/About.js
@@ -24,7 +24,7 @@ function AboutSection() {
         <div className="about-para">
           <p className="para">Hey there! I'm <span className="about-name">Guru Patel</span>, a passionate student at Ganpat University -  U. V. Patel College of Engineering, on a mission to become a top-notch  software engineer. For over 2.5 years, I've been deeply immersed in  coding, with a focus on tackling tough problems and bringing ideas to  life using the MERN stack.</p>
           <div className="downloadCv-btn">
-            <a href="/pdfs/MyResume.pdf" target="_blank" rel="noreferrer" download>Download CV</a>
+            <a href="/pdfs/resume_guru.pdf" target="_blank" rel="noreferrer" download>Download Resume</a>
           </div>
         </div>
       </div>
